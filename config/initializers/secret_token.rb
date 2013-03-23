dfe5b54dbf19d56052ce909be2eb0b6eb47adf22
Gabrielle Miller-Messner <millermessner@gmail.com>
2013-03-22 18:46:45 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yocona2::Application.config.secret_token = '521a887b547dd497e7003af046717a865973fe914dc75de242d1b7837f03c8b64bb880f60f3b0527f2fcb0e2513063872e56f249be92650830c16969bffde746'
