c220c47c8887c45050656373ef35d754b4bced31
Megumi Hora <megumihora@gmail.com>
2013-04-07 22:16:37 -0700

    Testing Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,14 +9,14 @@ Yocona2::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-   config.serve_static_assets = false
+   config.serve_static_assets = true
   # config.serve_static_assets = true #changed from false - MH 3/24
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-    config.assets.compile = true
+    config.assets.compile = false
   # config.assets.compile = true #changed from false - fixed "We're sorry, but something went wrong" Heroku error due to not precompiling static assets - MH 3/24
 
   # Generate digests for assets URLs
