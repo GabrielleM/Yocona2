77adb6ba0d57b28b17e1dfd2a9e343201fb89c60
Gabrielle Miller-Messner <millermessner@gmail.com>
2013-04-18 16:58:36 -0700

    changes to landing page text

---
+++ features/new_trip.feature
@@ -0,0 +1,7 @@
+Feature: Create New Trip
+
+
+Scenario:
+Given I am on the Yocona Trip website
+When I follow the "New Trip" link
+Then I should be taken to the Create New Trip page
---
+++ features/step_definitions/new_trip_steps.rb
@@ -0,0 +1,11 @@
+Given /^I am on (* page$)/ do |page|
+	#route
+	
+end
+When /^(?:*) (create new) / do |method|
+	#route
+end
+
+Then /should be taken (* page$) / do |page|
+	#route
+end
