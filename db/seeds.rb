914a07211d5cf670b47a3ba9e072c4a7ed6afbf2
Megumi Hora <megumihora@gmail.com>
2013-05-02 10:09:13 -0700

    Updated seed file. Running rake db:seed creates uniquness validation fail and throws error, but seeding process aactually works

--- db/seeds.rb
+++ db/seeds.rb
@@ -132,15 +132,12 @@ end
 
 
 
-
 gorge_id = 69
 gorge_id  = River.find_by_name("South Fork of the American, Gorge").id
 
 
 trips = [  
 			{:leader=> "Natasha Abeygunawardena", :agency=> "Cameron House", :agency_contact=> "", :num_participants=> '25', :num_guides=> '14', :flow=> "1400 cfs", :start_date=> "2012-07-14 00:00:00", :duration=> '1', :river_id=> gorge_id},
-
-			{:leader=> "Natasha Abeygunawarden", :agency=> "Cameron House", :agency_contact=> "", :num_participants=> '25', :num_guides=> '14', :flow=> "1400 cfs", :start_date=> "2012-07-14 00:00:00", :duration=> '1', :river_id=> gorge_id},
 
 			{:leader=> "Bill Weinberg", :agency=> "7 Tepees", :agency_contact=> "",:num_participants=> '18', :num_guides=> '10', :flow=> "1500 cfs", :start_date=> "2012-06-16 00:00:00", :duration=> '2', :summary=> "", :river_id=> gorge_id}
 		]
