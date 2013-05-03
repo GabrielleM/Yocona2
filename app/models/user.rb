147c4bda95f05b57c30021c28efcff6587be4372
Megumi Hora <megumihora@gmail.com>
2013-05-02 17:53:17 -0700

    Testing heroku email smtp server

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,7 +33,7 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :invitable 
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
@@ -46,5 +46,11 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  
+  after_create :deliver_signup_notification
+
+  def deliver_signup_notification
+    UserMailer.deliver_signup_notification(self)
+  end
   
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Yocona2::Application.configure do
 
   
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'http://powerful-hamlet-4616.herokuapp.com/' }
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
@@ -73,12 +73,13 @@ Yocona2::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "example.com",
+    domain: "http://powerful-hamlet-4616.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
   }
+
 
 
 
