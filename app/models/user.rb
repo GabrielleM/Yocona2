487cce9caee29da0d6edc5655ad92cbc8dee4f4a
Megumi Hora <megumihora@gmail.com>
2013-05-02 20:10:32 -0700

    Testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,7 +38,7 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable, 
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable #, :validatable
 
          # :confirmable
 
